475aa4773ac1f89816f2489972f8fb2223a94d70
mathias <mathias.gagnon@usherbrooke.ca>
2025-07-29 20:57:31 -0400

    better docstring

--- plugins/ingestion_plugin/hooks/base_storage_hook.py
+++ plugins/ingestion_plugin/hooks/base_storage_hook.py
@@ -5,28 +5,23 @@ class BaseStorageHook(ABC):
     @abstractmethod
     def copy_file(self, source_identifier: str, destination_identifier: str, new_name: str = None):
         """
-        Copy a file from source to destination.
+        Copies a file to a new parent folder in Google Drive.
 
-        Args:
-            source_identifier (str): Source file ID or path.
-            destination_identifier (str): Destination folder ID or path.
-            new_name (str, optional): New name for the copied file.
-
-        Returns:
-            Any: Result of the copy operation.
+        :param source_identifier: ID of the source file.
+        :param destination_identifier: ID of the destination folder.
+        :param new_name: Optional new name for the copied file.
+        :return: Metadata dictionary of the copied file.
         """
         pass
 
     @abstractmethod
-    def list_files(self, folder_id: str):
+    def list_files(self, folder_id: str) -> list:
         """
         List files in a folder.
 
-        Args:
-            folder_id (str): Folder ID.
+        :param folder_id: Folder ID.
 
-        Returns:
-            list: List of files in the folder.
+        :return: List of files in the folder.
         """
         pass
 
@@ -35,39 +30,33 @@ class BaseStorageHook(ABC):
         """
         Check if a file exists in a folder.
 
-        Args:
-            file_name (str): Name of the file to check.
-            folder_id (str): Folder ID.
+        :param file_name: Name of the file to check.
+        :param folder_id: Folder ID.
 
-        Returns:
-            bool: True if the file exists, False otherwise.
+        :return: True if the file exists, False otherwise.
         """
         pass
 
     @abstractmethod
-    def get_or_create_folder(self, folder_name: str, parent_folder_id: str):
+    def get_or_create_folder(self, folder_name: str, parent_folder_id: str) -> str:
         """
         Get a folder by name or create it if it doesn't exist.
 
-        Args:
-            folder_name (str): Name of the folder to get or create.
-            parent_folder_id (str): ID of the parent folder.
+        :param folder_name: Name of the folder to get or create.
+        :param parent_folder_id: ID of the parent folder.
 
-        Returns:
-            str: ID of the folder.
+        :return: ID of the folder.
         """
         pass
 
     @abstractmethod
-    def get_file_by_name(self, file_name: str, folder_id: str):
+    def get_file_by_name(self, file_name: str, folder_id: str) -> str:
         """
         Get a file by name from a specific folder.
 
-        Args:
-            file_name (str): Name of the file to retrieve.
-            folder_id (str): ID of the folder to search within.
+        :param file_name: Name of the file to retrieve.
+        :param folder_id: ID of the folder to search within.
 
-        Returns:
-            dict: File metadata if found, None otherwise.
+        :return: File metadata if found, None otherwise.
         """
         pass
